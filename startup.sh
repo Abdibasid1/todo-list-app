97ab47f30e543df5c7c92f27825bcbcf8516d1ad
Abdibasid1 <basidyaccub43@gmail.com>
2020-12-16 11:06:39 +0000

    new startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,28 @@
-a!/bin/bash
-cd /opt/todo-list-app
-sudo python3 -m venv venv
+#!/bin/bash
+sudo apt update
+sudo apt-get install python3-venv -y
+
+
+# Test Phase
+python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirement.txt
-python3 app.py
+pip3 install -r requirements.txt
+
+
+# pytest goes here
+
+
+# Deploy Phase
+
+
+# Make the installation directory
+sudo mkdir /opt/<todo-list-app>
+
+
+# Give jenkins user permissions for the installation directory
+sudo chown -R jenkins /opt/<todo-list-app>
+
+
+sudo systemctl daemon-reload
+sudo systemctl stop <todo-list-app.service > file
+sudo systemctl start <todo-list-app.service> file
