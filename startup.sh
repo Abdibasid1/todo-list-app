52a6552cafbcb4468a5e10a6b58c025e93997ba9
Abdibasid1 <basidyaccub43@gmail.com>
2020-12-16 11:17:37 +0000

    2nd new startup.sh

--- startup.sh
+++ startup.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 sudo apt update
 sudo apt-get install python3-venv -y
-
-
+ 
 # Test Phase
+cd $WORKSPACE
+ls -lah
+sudo cp -r $WORKSPACE /opt/jenkins
+sudo chown -R jenkins /opt/jenkins
+ 
+cd /opt/jenkins
 python3 -m venv venv
 source venv/bin/activate
+pip3 install wheel
+python setup.py bdist_wheel 
 pip3 install -r requirements.txt
-
-
+ 
 # pytest goes here
-
-
+ 
 # Deploy Phase
 
-
-# Make the installation directory
-sudo mkdir /opt/<todo-list-app>
-
-
-# Give jenkins user permissions for the installation directory
-sudo chown -R jenkins /opt/<todo-list-app>
-
-
 sudo systemctl daemon-reload
-sudo systemctl stop <todo-list-app.service > file
-sudo systemctl start <todo-list-app.service> file
+sudo systemctl stop todo-list-app.service
+sudo systemctl start todo-list-app.service
+ 
+python3 app.py
